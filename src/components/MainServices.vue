35ea97caf2beb3db6e5a77a4529038622df870d0
tatyshv <thernandez@ia.com.mx>
2021-06-12 19:09:22 -0500

    Feat: new design items servives

--- src/components/MainServices.vue
+++ src/components/MainServices.vue
@@ -1,21 +1,40 @@
 <template>
-  <section class="services container--fluid">
+  <section class="services container--fluid" id="services" v-on:scroll.passive="handleScroll" >
     <div class="filter-layout"></div>
-    <v-row>
-      <v-col cols="4"
-        class="card-service"
-        v-for="(item, index) in services"
-        :key="item.title"
-        :id="`item-${index}`"
-      >
-        <!--<figure e="width: 50px; margin: 0 auto;">-->
-          <span :class="`service-icons ${item.icon}`"></span>
-          <!-- <img max-width="96" :src="require(`@/assets/images/icons/${item.icon}`)"/>-->
-        
-        <h2 class="card-service-title">{{item.title}}</h2>
-        <p class="card-service-description">{{item.description}}</p>
-      </v-col>
-    </v-row>
+    <div class="services-wrap">
+      <v-row>
+        <h1 class="font-title title-component">OUR SERVICES</h1>
+      </v-row>
+      <v-row>
+        <v-col cols="6" style="z-index:2">
+          <div class="card-service services-left"
+            v-for="(item, index) in servicesLeft"
+            :key="item.title"
+            :id="`item-${index}`"
+          >
+            <span :class="`service-icons ${item.icon}`"></span>
+            <v-col>
+              <h2 class="card-service-title">{{item.title}}</h2>
+              <p class="card-service-description">{{item.description}}</p>
+            </v-col>
+          </div>
+        </v-col>
+
+        <v-col cols="6" style="z-index:2">
+          <div class="card-service services-right"
+            v-for="(item, index) in servicesRight"
+            :key="item.title"
+            :id="`item-${index}`"
+          >
+            <span :class="`service-icons ${item.icon}`"></span>
+            <v-col>
+              <h2 class="card-service-title">{{item.title}}</h2>
+              <p class="card-service-description">{{item.description}}</p>
+            </v-col>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
   </section>
 </template>
 
@@ -24,19 +43,38 @@
     name: 'Description',
     data(){
       return{
+        servicesLeft: [
+          { title: 'Removal & Replace Concrete', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-jackhammer' },
+          { title: 'Slab Foundation', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-architecture' },
+          { title: '10+ Years of Experience in Excavation ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-vehicle-excavator' },
+        ],
+        servicesRight : [
+          { title: 'Driveways & Walkways', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-trowel' },
+          { title: 'Retaining wall excavation', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-measure' },
+          { title: 'Footing Excavation ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-vehicle-dozer' },
+        ],
         services: [
-          { title: 'Removal & Replace Concrete', description: '', icon: 'icon-jackhammer' },
-          { title: 'Driveways & Walkways', description: '', icon: 'icon-trowel' },
-          { title: 'Slab Foundation', description: '', icon: 'icon-architecture' },
-          { title: 'Footing Excavation ', description: '', icon: 'icon-vehicle-dozer' },
-          { title: 'Driveway surfaces', description: '', icon: 'icon-mixer-truck' },
-          { title: 'Retaining wall excavation', description: '', icon: 'icon-measure' },
-          { title: '10+ Years of Experience in Excavation ', description: '', icon: 'icon-vehicle-excavator' },
-          { title: 'Top Materials', description: '', icon: 'icon-badge' },
-          { title: 'And More', description: '', icon: 'icon-project' },
+          { title: 'Removal & Replace Concrete', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-jackhammer' },
+          { title: 'Driveways & Walkways', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-trowel' },
+          { title: 'Slab Foundation', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-architecture' },
+          { title: 'Footing Excavation ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-vehicle-dozer' },
+          //{ title: 'Driveway surfaces', description: '', icon: 'icon-mixer-truck' },
+          { title: 'Retaining wall excavation', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-measure' },
+          { title: '10+ Years of Experience in Excavation ', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquam nisl sit amet venenatis venenatis.', icon: 'icon-vehicle-excavator' },
+          //{ title: 'Top Materials', description: '', icon: 'icon-badge' },
+          //{ title: 'And More', description: '', icon: 'icon-project' },
         ]
       }
-    }
+    },  
+    /* mounted(){
+      window.addEventListener('scroll', this.handleScroll);
+    },*/
+    methods: {
+      handleScroll(){
+        console.log('scroll')
+        document.getElementById('services').styles.backgroundPosition = '-50px' 
+      }
+    },
 
   }
 </script>
@@ -48,6 +86,24 @@
   background-position: center 
   background-size: cover
   padding: 3rem
+
+  .services-wrap
+    z-index: 2
+  .services-left
+    text-align: right
+    flex-direction: row-reverse
+  .services-right
+    text-align: left
+    flex-direction: row
+  .title-component 
+    font-size: 1.8rem 
+    color: white
+    text-align: center
+    z-index: 2
+    letter-spacing: 5px
+    margin: 2rem auto
+
+  
   .filter-layout 
     position: absolute 
     width: 100% 
@@ -58,14 +114,13 @@
     
   .card-service
     display: flex 
-    flex-direction: column 
     width: 100% 
-    padding: 3rem
+    padding: 1rem
     color: white 
-    text-align: center
     z-index: 2
     @include laptop 
-      padding: 3rem
+      padding: 1rem 2rem
+
     &-title 
       font-family: $font-title
       letter-spacing: .18rem
@@ -73,9 +128,9 @@
       color: white
       margin: 20px 0
       font-size: 1.1rem
-      width: 50%
-      max-width: 170px
-      margin: auto
+      //width: 50%
+      //max-width: 170px
+      //margin: auto
     &-description
       font-size: 1.2rem
       color: $white-80

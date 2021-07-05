52d2abc359170f67078bd366627c4550a0956647
Tatiana Hernández Villanueva <thernandez@ia.com.mx>
2021-07-04 21:37:17 -0500

    Feat: init store an vue router

---
+++ src/views/LatestJobs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container--fluid pageLatestJobs">
+    <div class="latestJobs-wrap container">
+      <v-row>
+        <h1 class="font-title title-component">LATEST JOBS</h1>
+      </v-row>
+      <v-row>
+        {{ latestJob }}
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['latestJob'])
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>

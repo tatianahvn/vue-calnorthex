d4300ed2b4caffb8778a5ad835a094f92416135e
tatysh <tatysakurahv@gmail.com>
2021-10-16 14:15:17 -0500

    Fix: new styles in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,39 +10,35 @@
 						:id="`item-${index}`"
 						@click="goToSection(item.id)"
 					>
-						<span>{{item.title}}</span>
+						<span class="cursor-pointer">{{item.title}}</span>
 					</a>
 				</div>
 			</v-col>
 			<v-col>
 				<div class="footer-contact d-flex flex-column align-center align-md-start">
 					<h1 class="mb-5">Contact Us</h1>
-					<div class="d-flex my-3">
-						<figure style="width:1.5rem; margin-right:10px">
-							<img src="../assets/images/icons/mail.svg"/>
-						</figure>
-						<span>calnorthex@gmail.com</span>
+					<div class="d-flex justify-center align-center my-2">
+						<div class="icon-email"></div>
+						<span class="cursor-pointer">calnorthex@gmail.com</span>
 					</div>
-					<div class="d-flex my-3">
-						<figure style="width:1.5rem; margin-right:10px">
-							<img src="../assets/images/icons/phone.svg"/>
-						</figure>
+					<div class="d-flex justify-center align-center my-2">
+						<div class="icon-phone"></div>
 						<span>(707) 490-7975</span>
 					</div>
 					<br>
-					<h1 class="mb-5">CONTRACTORS LICENSE</h1>
-					<span><strong> #1051560</strong></span>
+					<h1 class="mt-6 mb-3">CONTRACTORS LICENSE</h1>
+					<span style="letter-spacing: 2px;"><strong>#1051560</strong></span>
 				</div>
 			</v-col>
 			<v-col>
 				<div class="footer-contact d-flex flex-column align-center align-md-start">
 					<h1 class="mb-5">BUSINESS HOURS</h1>
-						<span>Monday to Friday - 8:00 AM to 6:00 PM</span>
-						<span>Saturday - By appoiment only</span>
-						<span>Sunday - Closed</span>
+						<span>Monday to friday: 8:00 AM to 6:00 PM</span>
+						<span>Saturday: Only by appoiment</span>
+						<span>Sunday: Closed</span>
 						<br><br>
 					<div class="d-flex flex-column align-center align-md-start justify-center">
-					<h1 class="mb-3">Social Media</h1> 
+					<h1 class="mt-6 mb-3">Social Media</h1> 
 					<div class="d-flex flex-row">
 						<v-btn :class="`menu-item item-${index} my-3 mr-5`"
 							v-for="(item, index) in socialMedia"
@@ -115,6 +111,7 @@ export default {
 	background-color: $dark-black!important
 	padding: 0
 	margin: 0
+	cursor: default
 	@include laptop 
 		height: 450px
 .footer-legal 
@@ -140,13 +137,15 @@ export default {
 
 	h1 
 		font-size: 1rem 
-		color: $soft-gray
+		color: $white-90
 		letter-spacing: 3px
 		text-transform: uppercase
 
 	span 
 		font-size: 1rem
-		letter-spacing: 2px
+		color: $white-80
+	
+	.cursor-pointer
 		cursor: pointer
 
 	.v-icon
@@ -172,4 +171,24 @@ export default {
 		display: flex 
 		flex-direction: column 
 		color: white
+
+	// Icons 
+
+	.icon-email
+		width: 25px
+		height: 25px
+		mask: url('../assets/images/icons/mail.svg')
+		mask-size: cover
+		background-color: $white-80
+		margin: auto
+		margin-right: 10px
+
+	.icon-phone
+		width: 25px
+		height: 25px
+		mask: url('../assets/images/icons/phone.svg')
+		mask-size: cover
+		background-color: $white-80
+		margin: auto
+		margin-right: 10px
 </style>
